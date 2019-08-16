9cb96b05adf1f3645599315669b254eb5d63df9e
Katie Rose <katiefelten@gmail.com>
2019-08-15 22:43:49 -0400

    :white_check_mark: MVP and some stretch complete

--- routers/recipeRouter.js
+++ routers/recipeRouter.js
@@ -1,13 +1,42 @@
 const router = require("express").Router();
+const db = require("../data/models/recipeBookModel");
 
 router.get("/", async (req, res) => {
   res.status(200).json({ message: "It's working. IT'S WORKING" });
 });
 
-router.get("/:id/shoppingList", async (req, res) => {});
+router.get("/:id/shoppingList", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const list = await db.getShoppingList(id);
+    if (!list) {
+      res.status(404).json({
+        message: `No ingredients for recipe with id ${id}.`
+      });
+    } else {
+      res.status(200).json(list);
+    }
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+});
 
-router.get("/:id/instructions", async (req, res) => {});
+router.get("/:id/instructions", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const list = await db.getInstructions(id);
+    if (!list) {
+      res.status(404).json({
+        message: `No instructions for recipe with id ${id}.`
+      });
+    } else {
+      res.status(200).json(list);
+    }
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+});
 
-router.get("/:id/recipes", async (req, res) => {});
+// router.get("/:id/recipes", async (req, res) => {});
 
 module.exports = router;
